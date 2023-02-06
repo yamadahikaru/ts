1a566f6f36751868b43efeff4be28361aa27805f
山田暉 <65005477+yamadahikaru@users.noreply.github.com>
2023-02-06 17:22:53 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,37 @@
+// TSの基本の型
+
+// boolean
+let bool:boolean = true ;
+
+//numer 数値
+let num : numeber = 0;
+
+// string文字
+let str:string = 'A';
+
+// Array　配列
+let arr1: Array<number> = [0,1,2,];
+let arr2: number[] = [0,1,2]
+
+
+// tuple
+let tuple : [number , string] = [0, "A"];
+
+// any
+let any1: any = false; 
+
+// void 
+cosnt funcA = ():void =>{
+  const test = 'test';
+}
+
+// null型
+let null1 :null = null ;
+
+// undefined
+let undefined1:undefined = undefined;
+
+//  object
+let obj1 : object = {} ;
+let obj2 :{id:number, name:string} = {id:0 , name:'aaaa'};
+
